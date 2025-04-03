51a02e90bccc651014985545c7ebbba52aa4df01
Shivank Tripathi <st.is.champion@gmail.com>
2025-04-03 21:30:12 +0530

    js started

---
+++ site33/script.js
@@ -0,0 +1,22 @@
+alert("hello")
+console.log("hello")
+
+var a = prompt("enter age")
+console.log(a)
+
+var b = confirm("yes no")
+
+if(b){
+    console.log("ty")
+}
+else{
+    console.log("bye")
+}
+
+console.log(typeof a, typeof b)
+
+
+
+// use let instead of var because var is globally scoped
+
+//typeof null = object
